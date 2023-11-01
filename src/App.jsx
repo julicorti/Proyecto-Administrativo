863afad9d32b23774561c34a8a90a5cef8f3a275
lospibes2 <thomas.babioet36@gmail.com>
2023-11-01 08:52:41 -0300

    Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import Login from "./Componentes/Login/login";
+import Año from "./Componentes/Años/Año";
+import Primero from "./Componentes/Divisiones/Primero";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Historial from "./Componentes/Historial/Historial";
+import Seleccion from "./Componentes/Divisiones/Seleccion";
+import HistorialAlumno from "./Componentes/HistorialAlumno/HistorialAlumno";
+function App() {
+  return (
+
+    <BrowserRouter>
+  
+          
+          <Routes>
+          <Route path="/" element={<Año/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/Primero" element={<Primero/>} />
+          <Route path="/Historial" element={<Historial/>} />
+          <Route path="/Ciclo_lectivo/:year" element={<Año/>}/>
+          <Route path="/Seleccion" element={<Seleccion/>}/>
+          <Route path="/HistorialAlumno" element={<HistorialAlumno/>}/>
+          </Routes>
+
+    </BrowserRouter>
+ /*    <div>
+    
+
+    </div> */
+  );
+}
+
+export default App;
